d0dde78041a73106e0ec601af57b1ce8d1cca8f8
bkyu <bkyu@aistudio.co.kr>
2023-05-13 10:36:27 +0900

    store에 saga미들웨어 설정

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,14 +1,17 @@
 import { combineReducers } from 'redux';
+// import {YOUTUBE, FLICKR} from './actionType';
 import * as names from './actionType';
 
 //{type: '문자열}, payload: 변경될 값
 const youtubeReducer = (state = { youtube: [] }, action) => {
 	switch (action.type) {
+		// case "YOUTUBE_START"
 		case names.YOUTUBE.start:
-			// case "YOUTUBE_START"
 			return state;
+		// case "YOUTUBE_SUCCESS"
 		case names.YOUTUBE.success:
 			return { ...state, youtube: action.payload };
+		// case YOUTUBE_FAIL
 		case names.YOUTUBE.fail:
 			return { ...state, youtube: action.payload };
 		default:
---
+++ src/Redux/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducer';
+import rootSaga from './saga';
+import createSagaMiddleware from '@redux-saga/core';
+
+//sagaMiddleware활성화
+const sagaMiddleware = createSagaMiddleware();
+
+//store에 reducer데이터를 전달할때 sagaMiddleware를 적용해서 전달
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+//적용된 sagaMiddleware로 부터 rootSaga함수 호출
+sagaMiddleware(rootSaga);
+
+//saga 미들웨어가 적용된 reducer데이터를 store에 저장하고 export
+export default store;
